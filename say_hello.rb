caf946aa9e58d7e2962b389390699c197327e775
Duc Le Dang <51703059@student.tdtu.edu.vn>
2020-02-01 04:30:08 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,7 @@
 # Build your say_hello method here
 def say_hello(name = "Duc")
-  puts "Hello #{name}"
+  if (name) puts "Hello Ruby Programmer"
+  else puts "Hello #{name}"
 end
 
 say_hello("Cao")
